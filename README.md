96fcface87f69dbe30c1360746bcceec47068dd5
Gideon Ritter <gideon.ritter@enymo.com>
2023-06-12 12:27:28 +0200

    Finish adding documentation.

---
+++ README.md
@@ -0,0 +1,77 @@
+# React Resource Hook
+
+React Hook for interacting with rest apis.
+
+## Installation
+The library may be installed from npm using
+```bash
+npm i @enymo/react-resource-hook
+```
+
+## Features
+* Access REST resources using a simple, laravel-inspired naming scheme and a React-friendly declarative syntax
+* Create, update and destroy resources with easy-to-use methods
+* Upload files without needing to manually convert you payload to FormData
+* Easily incorporate WebSockets to keep your frontend up-to-date as data is changed by other processes.
+
+## Usage
+In order to use the ```useResource``` hook in your components, you app has to be wrapped with the ResourceProvider to configure the hook
+```
+import React from "react";
+import { RouteFunction, ResourceProvider } from "@enymo/react-resource-hook";
+import route from "ziggy-js";
+import axios from "axios";
+
+function App() {
+    return (
+        <ResourceProvider value={{axios: axios, routeFunction: route as RouteFunction, reactNative: false}}>
+            {/* Your app here */}
+        </ResourceProvider>
+    )
+}
+```
+*NOTE* If you're using ziggy-js for the route function: As of 06/12/2023, the typing for ziggy-js is incorrect, but it's implementation isn't. Because of this, ```route``` has to be cast to ```RouteFunction``` to be used with the resource hook.
+
+You can than access resource in your components like this:
+```
+import useResource from "@enymo/react-resource-hook";
+import React from "react";
+
+export default function Component() {
+    const [items, {loading, store, update, destroy, extra, refresh}] = useResource<{
+        id: number,
+        text: string
+    }>("my-resource");
+
+    if (loading) {
+        return null;
+    }
+
+    return (
+        <div>
+            {items.map(({id, text}) => (
+                <button
+                    key={id}
+                    onClick={() => update(id, {
+                        text: "Updated!"
+                    })}
+                >{text}</button>
+            ))}
+        </div>
+    )
+}
+```
+
+### Configuration
+The ```ResourceProvider``` allows to configure the behavior of the resource hook for all components placed within. The following options may be passed to the ```value```-prop.
+| Option        | Description                                                                                                                                                                                                                    |
+|---------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| axios         | You can set a custom axios instance to be used for all requests the hook makes.<br>If you don't need this, you can simply pass the global axios import                                                                         |
+| routeFunction | The resource hook was developed to be used with [Laravels](https://github.com/laravel/laravel) [resource routes](https://laravel.com/docs/10.x/controllers#resource-controllers) and [ziggy-js](https://github.com/tighten/ziggy),<br>but any function with the same signature as ziggy-js' 'route' may be used                                                            |
+| reactNative   | Should be set to true when using this library in a react native project.<br>This slightly alters the behavior of the hooks form data converter (see below) to account<br>for react natives unique way of handling file uploads |
+
+### Resource naming and routing
+WIP
+
+### Options
+WIP (jsdoc available)
--- src/js/index.ts
+++ src/js/index.ts
@@ -5,7 +5,7 @@ import { createContext, useCallback, useContext, useEffect, useMemo, useState } 
 import { DeepPartial } from "ts-essentials";
 import { filter, identity, isNotNull, objectToFormData, pruneUnchanged, randomString, requireNotNull } from "./util";
 
-type Handler<T, U> = (item: T, prev: U | null) => U;
+type Handler<T, U> = (item: T, prev: U | null) => U | null;
 type UpdateMethod = "on-success" | "immediate" | "local-only";
 type Param = string|number|boolean;
 export type Params = {[param: string]: Param|Param[]|Params}
@@ -59,6 +59,7 @@ interface OptionsCommon<T extends Resource, U> {
     transformer?(item: any): DeepPartial<T> | Promise<DeepPartial<T>>,
     /**
      * The inverse of the transformer, serializing data for sending to the api. See transformer for details.
+     * @param item A single item to be serialized for the api
      */
     inverseTransformer?(item: DeepPartial<U>): any | Promise<any>
 }
@@ -66,17 +67,59 @@ interface OptionsCommon<T extends Resource, U> {
 type OnDestroyedList<T extends Resource> = (id: T["id"], prev: T[]) => T[]
 
 interface OptionsList<T extends Resource, U> extends OptionsCommon<T, U> {
+    /**
+     * If true, the hook expects extra data to be returned from the initial get request to consist of an 'extra' field containing any extra data to be returned
+     * and a 'data' field containing the actual resource data.
+     */
     withExtra?: boolean,
+    /**
+     * Called every time a new item is created for the current resource (either using the 'store' method or by receiving the respective socket event)
+     * If its omitted or returns null, the created item will simply be appended to the current state, but the method may also return the new state to override this behavior
+     * @param item The item that has been created (already transformed)
+     * @param prev The previous state
+     * @returns The new state or null if default behavior should be used
+     */
     onCreated?: Handler<T, T[]>,
+    /**
+     * Called every time an item is updated for the current resource (either using the 'update' method or by receiving the respective socket event)
+     * If its omitted or returns null, an item in the current state with the same id or ephermeral state will be updated with the new values (if it exists),
+     * but the method may also return the new state to override this behavior
+     * @param item The (partial) item that has been updated (already transformed)
+     * @param prev The previous state
+     * @returns The new state or null if default behavior should be used
+     */
     onUpdated?: Handler<DeepPartial<T>, T[]>,
+    /**
+     * Called every time an item is destroyed for the current resource (either using the 'destroy' method or by receiving the respective socket event)
+     * If its ommited or returns null, an item in the current state with the same id is removed (if it exists),
+     * but the method may also return the new state to override this behavior
+     * @param id The id of the item that has been destroyed
+     * @param prev The previous state
+     * @returns The new state or null if default behavior should be used
+     */
     onDestroyed?: OnDestroyedList<T>
 }
 
 type OnDestroyedSingle<T extends Resource> = (item: T["id"]) => void;
 
 interface OptionsSingle<T extends Resource, U> extends OptionsCommon<T, U> {
+    /**
+     * The id of the resource to be requested or 'single' if it is a [singleton resource]{@link https://www.google.de}
+     */
     id: T["id"] | "single",
+    /**
+     * Called whenever the current item is updated.
+     * If its omitted or returns null, the item will simply be updated using the new values,
+     * but the method may also return the new state to override this behavior
+     * @param item The (partial) updated item
+     * @param prev The previous state
+     * @returns The new state or null if default behavior should be used
+     */
     onUpdated?: Handler<DeepPartial<T>, T>,
+    /**
+     * Called whenever the current item is destroyed.
+     * @param id The id of the destroyed item
+     */
     onDestroyed?: OnDestroyedSingle<T>
 }
 
@@ -89,27 +132,109 @@ interface OptionsImplementation<T extends Resource, U> extends OptionsCommon<T, 
 }
 
 interface ReturnCommon<T extends Resource, U> {
+    /**
+     * Whether the current resource is still being fetched after initial render or parameter change
+     */
     loading: boolean,
+    /**
+     * Stores a new item in the current resource
+     * @param item The item to be stored
+     * @param updateMethod The update method to be used
+     *  'on-success' will only update the resource in the frontend once the backend returns a successful response. The created item will contain an id and potentially field filled by the backend
+     *  'immediate' will update the resource in the frontend immediately while also sending the request to the backend. 
+     *      The created item will have the id set to null and not contain any fields that would be filled by the backend. Once the backend emits a 'created' event (sockets only), the resource will be updated
+     *  'local-only' will update the resourcei in the frontend immediately and not send a request to the backend at all.
+     * @param config An AxiosRequestConfig may be passed to be used for the request
+     * @returns The created resource.
+     */
     store: (item?: DeepPartial<U>, updateMethod?: UpdateMethod, config?: AxiosRequestConfig) => Promise<T>,
+    /**
+     * Fully refreshed the resource by sending the initial get request again.
+     * @param config An axios request config to be used to the request
+     * @returns A void promise that resolves when the refresh is complete.
+     */
     refresh: (config?: AxiosRequestConfig) => Promise<void>
 }
 
 export interface ReturnList<T extends Resource, U, V> extends ReturnCommon<T, U> {
+    /**
+     * Updates an existing item for the current resource
+     * @param id The id of the item to update
+     * @param update Partial item. Omitted fields are considered unchanged.
+     * @param updateMethod The update method to be used
+     *  'on-success' will only update the resource in the frontend once the backend returns a successful response.
+     *      The frontend will be updated using the data from the backends response (which might be different from the data sent in the request)
+     *  'immediate' will update the resource in the frontend immediately while also sending the request to the backend. The frontend will be updated using
+     *      only the data provided in the request, but a subsequent 'updated' event (socket only) may update the item again once the requests succeeeds
+     *  'local-only' will only update the frontend with the values provided, without sending any request to the backend
+     * @param config An AxiosRequestConfig may be passed to be used for the request
+     * @returns A void promise that resolves once an 'on-success' request is complete or immediately otherwise
+     */
     update: (id: T["id"], update: DeepPartial<U>, updateMethod?: UpdateMethod, config?: AxiosRequestConfig) => Promise<void>,
+    /**
+     * Destroys an item for the current resource
+     * @param id The id of the item to destroy
+     * @param updateMethod The update method to be used
+     *  'on-success' will only remove the item in the frontend once the backend returns a successful response.
+     *  'immediate' will update the frontend immedately while also sending the request to the backend
+     *  'local-only' will only remove the item in the frontend
+     * @param config An AxiosRequestConfig may be passed to be used for the request
+     * @returns A void promise that resolves once an 'on-success' request is complete or immediately otherwise
+     */
     destroy: (id: T["id"], updateMethod?: UpdateMethod, config?: AxiosRequestConfig) => Promise<void>,
+    /**
+     * Extra data returned from the initial get request. Requires 'withExtra' option to be set to 'true'. See documentation
+     * for this option for the expected response format
+     */
     extra: V | null
 }
 
 export interface ReturnSingle<T extends Resource, U = T> extends ReturnCommon<T, U> {
+    /**
+     * Updates the current item
+     * @param update Partial item. Omitted fields will be considered unchanged.
+     * @param updateMethod The update method to be used
+     *  'on-success' will only update the resource in the frontend once the backend returns a successful response.
+     *      The frontend will be updated using the data from the backends response (which might be different from the data sent in the request)
+     *  'immediate' will update the resource in the frontend immediately while also sending the request to the backend. The frontend will be updated using
+     *      only the data provided in the request, but a subsequent 'updated' event (socket only) may update the item again once the requests succeeeds
+     *  'local-only' will only update the frontend with the values provided, without sending any request to the backend
+     * @param config An AxiosRequestConfig may be passed to be used for the request
+     * @returns A void promise that resolves once an 'on-success' request is complete or immediately otherwise
+     */
     update: (update: DeepPartial<U>, updateMethod?: UpdateMethod, config?: AxiosRequestConfig) => Promise<void>,
+    /**
+     * Destroys the current item
+     * @param updateMethod The update method to be used
+     *  'on-success' will only remove the item in the frontend once the backend returns a successful response.
+     *  'immediate' will update the frontend immedately while also sending the request to the backend
+     *  'local-only' will only remove the item in the frontend
+     * @param config An AxiosRequestConfig may be passed to be used for the request
+     * @returns A void promise that resolves once an 'on-success' request is complete or immediately otherwise
+     */
     destroy: (updateMethod?: UpdateMethod, config?: AxiosRequestConfig) => Promise<void>
 }
 
 export type RouteFunction = (route: string, params?: Params) => string
 
 const Context = createContext<{
+    /**
+     * The axios instance to be used for all requests by the resource hook. If you don't need a custom instance,
+     * you can pass the global axios import instead
+     */
     axios: AxiosInstance,
+    /**
+     * The route function to be used. The resource hook was developed to be used with laravel and ziggy-js, but any route function
+     * that uses the same function signature can be used.
+     * 
+     * NOTE: As of 06/12/2023, ziggy-js types are incorrect, even though the implementation isn't. Therefore, ziggys 'route' has to
+     * be cast to RouteFunction to be used with teh resource hook
+     */
     routeFunction: RouteFunction,
+    /**
+     * Should be set to true if the hook is used in a react native project. This slightly changes the behavior of the hooks form data
+     * converter to account for react natives unique handling of file uploads.
+     */
     reactNative?: boolean
 } | null>(null);
 
